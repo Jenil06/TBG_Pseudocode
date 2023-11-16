b8ec236276ffa1f4b0825e592fd3a0c87822a080
Jenil06 <145785147+Jenil06@users.noreply.github.com>
2023-11-16 09:06:54 -0600

    Add files via upload

---
+++ TBG_Pseudocode-Jenil_Patel.py
@@ -0,0 +1,37 @@
+#TBG Pseudocode - Jenil Patel
+#Nov. 16, 2023
+#This is a pseudocode to plan ahead for when I make my TBG
+
+#Intorduce the characters in a numerical list
+#Ask the user who they would like to play as
+#Print their character
+
+#Print summery of a conversation between friends about haunted house
+#Ask user if they would like to go to haunted house with thier friends
+
+#If they say yes
+    #Print You went home and grabed a few supplies
+    #Show the items in a numerical list
+    
+    #Print it is later that evening
+    #Print You and your friends are out side the haunted house
+    #Ask how they will enter the haunted house
+
+
+
+#If they say no
+    #Print You go home and rest that evening
+    
+    #Next morning
+    #Print at school there is an anounccment
+    #Print they say all your frineds are missing
+    #Ask do you go looking for them or not say a word
+    
+    #If they go looking
+        #They find thier friend in a coma
+        #Print you blame yourself for not going
+        # and for the rest of the school year you make no friends
+    
+    #If they stay quite
+        #Print all your friends are never found
+        #Print you blame yourself for thire the mess and never make friends
